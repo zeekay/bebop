e8c569b275c6b85a0b6931d852f3c460b95ff8b5
zeekay <zeekayy@gmail.com>
2012-03-14 15:30:47 -0700

    Make sure and include script tags when injecting bebop.js in middleware.

--- bebop/middleware.py
+++ bebop/middleware.py
@@ -4,7 +4,8 @@ from django.conf import settings
 HOST = getattr(settings, 'BEBOP_WEBSOCKET_HOST', '127.0.0.1')
 PORT = getattr(settings, 'BEBOP_WEBSOCKET_PORT', '9000')
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bebop.js')) as f:
-    RELOADER_SCRIPT = f.read()
+    RELOADER_SCRIPT = ''.join(('<script type="text/javascript">', f.read(), '</script>'))
+
 RELOADER_SCRIPT.replace('127.0.0.1', HOST)
 RELOADER_SCRIPT.replace('9000', PORT)
 
