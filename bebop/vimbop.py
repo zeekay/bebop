c1ef8c8c4e97260e2ce8c05d507a12a4577bacd2
zeekay <zeekayy@gmail.com>
2012-03-19 10:25:48 -0500

    Added timeout, updated identifier regex to match chained functions.

--- bebop/vimbop.py
+++ bebop/vimbop.py
@@ -8,12 +8,13 @@ except ImportError:
     pass
 
 
-IDENTIFIER_REGEX = re.compile('[$a-zA-Z_][0-9a-zA-Z_$.]*')
+IDENTIFIER_REGEX = re.compile(r'[$a-zA-Z_][()0-9a-zA-Z_$.\'"]*')
 
 class Client(object):
     def __init__(self, host='127.0.0.1', port=9128):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
+        self.socket.settimeout(5)
 
     def send(self, data):
         self.socket.sendall(data)
