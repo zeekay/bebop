2fb3ffa002ec1c1275c9b979cffcbbf052ac0641
zeekay <zeekayy@gmail.com>
2012-03-23 03:28:04 -0500

    Updated client.

--- bebop/client.py
+++ bebop/client.py
@@ -5,7 +5,6 @@ import socket
 class Client(object):
     '''
     Client object for communicating with Bebop's TCP interface.
-
     '''
     def __init__(self, host='127.0.0.1', port=9128):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,13 +30,23 @@ class Client(object):
         self.socket.close()
 
 
-def eval(*args):
+def eval(code):
     '''
     Sends code to Bebop, which will be run in browser.
     '''
-    code = ' '.join(args)
-    client = Client()
-    client.send(json.dumps({'evt': 'eval', 'msg': code}))
-    result = client.recv()
-    client.close()
-    print result
+    c = Client()
+    c.send(json.dumps({'evt': 'eval', 'msg': code}))
+    result = c.recv()
+    c.close()
+    return result
+
+
+def complete(obj):
+    '''
+    Asks bebop to return a list of properties on a given object.
+    '''
+    c = Client()
+    c.send(json.dumps({'evt': 'complete', 'msg': obj}))
+    result = c.recv()
+    c.close()
+    return result
