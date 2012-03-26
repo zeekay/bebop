3f01bb5da72984ae7841237ac375071ba5897880
zeekay <zeekayy@gmail.com>
2012-03-26 03:09:57 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-setup(name='Bebop',
+setup(name='bebop-server',
       version='0.1',
+      url="https://github.com/zeekay/bebop",
       author='Zach Kelling',
+      author_email='zeekayy@gmail.com',
       packages=['bebop', 'bebop.management.commands'],
-      description='WebSocket web app reloader for rapid development',
+      description='A tool for rapid web development',
       install_requires=['autobahn', 'twisted', 'watchdog'],
       scripts=['bin/bebop'],
 )
